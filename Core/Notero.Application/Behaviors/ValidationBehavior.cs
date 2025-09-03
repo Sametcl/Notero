72137cd51b72da1dcbfce2f9ea1fa1e6afc51613
Sametcl <samet2016.2022@gmail.com>
2025-09-03 21:13:46 +0300

    validation behavior yapisi kuruldu .

---
+++ Core/Notero.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notero.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : class
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                if (failures.Any())
+                {
+                    var errorDetails = failures.GroupBy(x=>x.PropertyName)
+                        .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage).ToArray()).ToList();
+                    throw new ValidationException(failures);
+                }
+            }
+            return await next(cancellationToken);    
+        }
+    }
+}
--- Core/Notero.Application/Extensions/ServiceRegistrations.cs
+++ Core/Notero.Application/Extensions/ServiceRegistrations.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using Notero.Application.Behaviors;
 using System.Reflection;
 
 namespace Notero.Application.Extensions
@@ -9,8 +10,12 @@ namespace Notero.Application.Extensions
         public static void AddApplication(this IServiceCollection services)
         {
             var assembly = Assembly.GetExecutingAssembly();
-            services.AddAutoMapper(cfg => cfg.AddMaps(assembly)); 
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+            services.AddAutoMapper(cfg => cfg.AddMaps(assembly));
+            services.AddMediatR(cfg => 
+            {
+                cfg.RegisterServicesFromAssembly(assembly);
+                cfg.AddOpenBehavior(typeof( ValidationBehavior<,>));
+            });
             services.AddValidatorsFromAssembly(assembly);
         }
     }
